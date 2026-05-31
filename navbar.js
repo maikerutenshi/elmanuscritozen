7aa4ec356c79a4213c892ddfdbe57ae833a468df
Miguel <maikerutenshi@gmail.com>
2026-05-31 10:21:48 +0200

    Unificado diseño: CSS base y sistema de navegacion inyectada

--- navbar.js
+++ navbar.js
@@ -2,18 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
     <nav class="navbar">
       <div class="nav-inner">
-        <a href="/" class="nav-logo">
+        <a href="index.html" class="nav-logo">
           <div class="nav-logo-symbol"><img src="enso.png" alt="Enso" class="enso-img"></div>
-          <span class="nav-logo-text">El manuscrito Zen</span>
+          <span class="nav-logo-text">El Manuscrito Zen</span>
         </a>
         <div class="nav-links">
-          <a href="/" class="nav-link">Inicio</a>
-          <a href="/archivo.html" class="nav-link">Archivo</a>
-          <a href="/sobre-este-sitio" class="nav-link">Sobre este sitio</a>
-          <a href="/admin" class="nav-link">Administración</a>
+          <a href="index.html" class="nav-link">Inicio</a>
+          <a href="archivo.html" class="nav-link">Archivo</a>
+          <a href="dojo.html" class="nav-link">Dojo Virtual</a>
+          <a href="sobre.html" class="nav-link">Sobre este sitio</a>
         </div>
       </div>
     </nav>`;
-    
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 });
