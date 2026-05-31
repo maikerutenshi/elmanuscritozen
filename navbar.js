323fc002c9a2d8b92a0edc0c0483898df9e11205
Miguel <maikerutenshi@gmail.com>
2026-05-31 11:15:34 +0200

    Corrección: sincronizado nombre de archivo admin.html con el enlace del menú

--- navbar.js
+++ navbar.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
           <a href="archivo.html" class="nav-link">Archivo</a>
           <a href="dojo.html" class="nav-link">Dojo Virtual</a>
           <a href="sobre.html" class="nav-link">Sobre este lugar</a>
-          <a href="administracion.html" class="nav-link">Administración</a>
+          <a href="admin.html" class="nav-link">Administración</a>
         </div>
         <button class="nav-hamburger" id="nav-hamburger">
             <span></span>
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <a href="archivo.html" class="nav-mobile-link">Archivo</a>
         <a href="dojo.html" class="nav-mobile-link">Dojo Virtual</a>
         <a href="sobre.html" class="nav-mobile-link">Sobre este lugar</a>
-        <a href="administracion.html" class="nav-mobile-link">Administración</a>
+        <a href="admin.html" class="nav-mobile-link">Administración</a>
     </div>`;
     
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
