6452f42e5c2299874dbd64f43fa435b71ce633c9
Miguel <maikerutenshi@gmail.com>
2026-06-06 14:59:30 +0200

    Configurar Firebase para comentarios

---
+++ comments-config.js
@@ -0,0 +1,9 @@
+// Firebase solo para comentarios (app: ManuscritoZenWeb)
+const COMMENTS_FIREBASE = {
+  apiKey: 'AIzaSyAzSR82Pqvl5oSxVrfRAws9DbqsvVE0gM0',
+  authDomain: 'el-manuscrit-zen.firebaseapp.com',
+  projectId: 'el-manuscrit-zen',
+  storageBucket: 'el-manuscrit-zen.firebasestorage.app',
+  messagingSenderId: '876463343177',
+  appId: '1:876463343177:web:7cbe73049b8a179f736198',
+};
