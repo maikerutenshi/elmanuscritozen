f69fe3c8b4f4707a59e4a3c10217438269c6d781
Miguel <maikerutenshi@gmail.com>
2026-06-06 22:39:17 +0200

    Nueva app Firebase ManuscritoZenComentarios

--- comments-config.js
+++ comments-config.js
@@ -1,11 +1,11 @@
-// Firebase solo para comentarios (app: ManuscritoZenWeb)
+// Firebase solo para comentarios (app: ManuscritoZenComentarios)
 const COMMENTS_FIREBASE = {
-  apiKey: 'AIzaSyAzSR82PqvI5oSxVrfRAws9DbqsvVEOgM0',
+  apiKey: 'AIzaSyBPYWd81Tw7w67fAnBVOPiff1rl-BWB58g',
   authDomain: 'el-manuscrit-zen.firebaseapp.com',
   projectId: 'el-manuscrit-zen',
   storageBucket: 'el-manuscrit-zen.firebasestorage.app',
   messagingSenderId: '876463343177',
-  appId: '1:876463343177:web:7cbe73049b8a179f736198',
+  appId: '1:876463343177:web:72f6d7e17da36b72736198',
   // Correo con el que TÚ entras (Google o correo) para moderar comentarios:
   adminEmail: 'mbardisa@gmail.com',
 };
